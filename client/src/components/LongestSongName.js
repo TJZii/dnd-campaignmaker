ac254d68a19a97c5099b5fa61c787ec69ecda7fd
ziitania <tjhaweszii@gmail.com>
2021-07-21 15:11:26 -0700

    added extra info on the longest name page

--- client/src/components/LongestSongName.js
+++ client/src/components/LongestSongName.js
@@ -1,23 +1,32 @@
 import React, {useState, useEffect } from 'react'
+import ReactPlayer from 'react-player';
 
 const LongestSongName = (props) => {
-    const [songs, setSongs] = useState([])
+    const [longestSong, setSongs] = useState({})
+
 
     useEffect(()=> {
-        fetch('/longestSong')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data){
-                setSongs(data.name)
-        }})
+            fetch('/longestSong')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setSongs(data)
+            })
+            .then(
+                console.log(longestSong),
+                console.log(longestSong.playlist),
+                // console.log(longestSong.playlist.name)
+            )
     }, [])
 
 
     return (
         <div>
-            <h1>"{songs}"</h1>
+            <h1>"{longestSong.name}" by {longestSong.artist}</h1>
+            <ReactPlayer url={longestSong.url}/>
             <h2>This is the longest song name that any user has added to a playlist!</h2>
+            <br></br>
+            {/* <h3>Song added to a playlist named {longestSong.playlist.name} by a user</h3> */}
         </div>
     )
 }
